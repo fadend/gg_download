c8efa0e874809123cd8ee36fb908e6abefb87f1c
David Faden (revfad.com) <dfaden@gmail.com>
2024-12-15 23:32:52 -0800

    Fetch full index and add retries for downloads.
    I added retries to deal with "tab navigated before capture could complete."
    errors; these seem impossible with my current understanding of the API
    (since we don't navigate away till we're done awaiting the blob) but
    probably some further digging on that is needed. (On the other hand,
    my immediate task for this script is almost finished.)
    
    Unfortunately, I had a bug in my code while doing a test run so retries
    weren't fully tested. One TODO would be to add a delay between retries.

--- scripts/service-worker.js
+++ scripts/service-worker.js
@@ -29,6 +29,19 @@ function findPostUrls() {
     return new Promise((resolve, reject) => { resolve(uniqueUrls); });
 }
 
+function clickNext() {
+    const nextButton = document.querySelector('[aria-label="Next page"]');
+    let clicked = false;
+    if (nextButton && window.getComputedStyle(nextButton).cursor === 'pointer') {
+        console.log('Clicking Next page');
+        nextButton.click();
+        clicked = true;
+    } else {
+        console.log('Not clicking Next page');
+    }
+    return new Promise((resolve, reject) => { resolve(clicked); });
+}
+
 // Return a Promise that will resolve after the given number of milliseconds.
 function delay(millis) {
     return new Promise((resolve, reject) => {
@@ -36,44 +49,82 @@ function delay(millis) {
     });
 }
 
-chrome.action.onClicked.addListener(async (tab) => {
+async function getPostUrlsAcrossIndex(tab) {
     if (!/^https:\/\/groups\.google\.com\/g\/[^\/]+$/.test(tab.url)) {
-        console.log('Can only run on index page for a Google Group, not ', tab.url);
-        return;
+        throw new Error(`Can only run on index page for a Google Group, not ${tab.url}`);
     }
-    await chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        func: fullyLoaded
-    });
-    const urlsResult = await chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        func: findPostUrls
-    });
-    const urls = urlsResult[0].result;
+    const urls = new Set();
+    do {
+        await chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            func: fullyLoaded
+        });
+        const urlsResult = await chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            func: findPostUrls
+        });
+        for (let url of urlsResult[0].result) {
+            urls.add(url);
+            console.log(`Found ${url}`);
+        }
+        const clickedResult = await chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            func: clickNext
+        });
+        if (!clickedResult[0].result) {
+            break;
+        }
+        await delay(700 + 500 * Math.random());
+    } while (true);
+    return Array.from(urls);
+}
+
+async function downloadUrlCapture(tab_id, url) {
+    try {
+        chrome.tabs.update(tab_id, { url });
+        // Wait a teeny bit for the update to actually happen.
+        // TODO: is this actually helpful?
+        await delay(10);
+        await chrome.scripting.executeScript({
+            target: { tabId: tab_id },
+            func: fullyLoaded
+        });
+        const blob = await chrome.pageCapture.saveAsMHTML({ tabId: tab_id });
+        const content = await blob.text();
+        const dataUrl = "data:application/x-mimearchive;base64," + btoa(content);
+        const postName = url.split('/').pop();
+        await chrome.downloads.download({ url: dataUrl, filename: `${postName}.mhtml` });
+    } catch (error) {
+        console.log(`Failed to download ${url}`, error);
+        return false;
+    }
+    return true;
+}
+
+chrome.action.onClicked.addListener(async (tab) => {
+    const urls = await getPostUrlsAcrossIndex(tab);
     console.log(`Found ${urls.length} URLs`);
+    console.log(`URLs: ${urls}`);
     let first = true;
     let downloadCount = 0;
     for (let url of urls) {
         console.log('Processing URL ', url);
         if (!first) {
             // Add some delay to be friendlier to Google's servers.
-            await delay(5000 + 10000 * Math.random());
+            await delay(7000 + 23000 * Math.random());
         } else {
             first = false;
         }
-        chrome.tabs.update(tab.id, { url });
-        // Wait a teeny bit for the update to actually happen.
-        // TODO: is this actually helpful?
-        await delay(10);
-        await chrome.scripting.executeScript({
-            target: { tabId: tab.id },
-            func: fullyLoaded
-        });
-        const blob = await chrome.pageCapture.saveAsMHTML({ tabId: tab.id });
-        const content = await blob.text();
-        const dataUrl = "data:application/x-mimearchive;base64," + btoa(content);
-        const postName = url.split('/').pop();
-        await chrome.downloads.download({ url: dataUrl, filename: `${postName}.mhtml` });
+        let successful = false;
+        for (let tries = 0; tries < 3; tries++) {
+            successful = downloadUrlCapture(tab.id, url);
+            if (successful) {
+                break;
+            }
+        }
+        if (!successful) {
+            console.log(`All attempts for ${url} failed. Continuing.`);
+        }
         downloadCount++;
         console.log('Download #', downloadCount);
     }
